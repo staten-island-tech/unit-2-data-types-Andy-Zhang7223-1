08a331c25f806c4938b1004350217933dc826042
Andy-Zhang7223 <andyz127@nycstudents.net>
2024-09-16 10:13:16 -0400

    Commiting Unit 2

--- Data Types.py
+++ Data Types.py
@@ -1,9 +1,26 @@
-x = 3
-y = float(3)
-print(x,y)
-values = [1,2.23,5,7,2,30,15]
+""" x=(3)
+y=float(3)
+print(x,y) """
+""" values = [1,2.23,5,7,2,30,15]
 print(values)
+
 for i in values:
     print(i)
     print(values[0])
-print(values[6])
+print(values[6]) """
+
+""" x = "this is a working thing"
+y= x.split( )
+z = y[1]
+v = y[0]
+w = y[3]
+h = z[0]
+print(y)
+print(z)
+print(v)
+print(w)
+print(h) """
+
+
+x = input('TYPE HERE')
+y = x.split(" ")
